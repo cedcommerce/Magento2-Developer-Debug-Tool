c33f04eba4de131449f92f4ebe7579df301bf03c
mallika <mallikaverma@cedcoss.com>
2016-07-16 12:38:08 +0530

    change in composer and some minor changes

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 
 \Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+   \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Ced_DevTool',
     __DIR__
 );
